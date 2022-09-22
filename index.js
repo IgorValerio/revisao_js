1cc5a93b440002eeeab66516abb952ae0519b408
igor <igor.valerio@hotmail.com>
2022-09-22 01:22:54 -0300

    revisao array

---
+++ index.js
@@ -0,0 +1,28 @@
+//           012345678910
+const nome = 'Luiz otávio';
+//console.log(nome[10])
+
+//                0        1        2
+const alunos = ['Carlos', 'Maria', 'Joao'];
+alunos = 123;
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+//console.log(alunos[50]);
+//delete alunos[1];
+//console.log(alunos)
+//console.log(alunos[1]) //undefined
+//const removido = alunos.shift();
+//console.log(removido)
+//console.log(alunos)
+//alunos.unshift('Luiza');//acidiona no começo
+//alunos.push('Luiza');
+//[alunos.length] = 'Luiza';
+//alunos[alunos.length] = 'Fabio';
+//alunos[alunos.length] = 'Luana';
+//alunos[0] = 'Eduardo' //Altera
+//alunos[3] = 'Luiza'
+//console.log(alunos)
+//console.log(alunos[2]) 
+//
